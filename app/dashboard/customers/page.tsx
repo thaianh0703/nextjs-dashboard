5951a673b69f30326a8ce793a991aa8e886a5347
thaianh0703 <54608731+thaianh0703@users.noreply.github.com>
2024-11-14 19:10:47 +0700

    thaianh

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import Image from "next/image";
 export default function Page() {
-  return <p>Customers Page</p>;
+  return <p>
+    Customers Page
+    <Image
+      src="/images/thaianh.png"
+      width={1089}
+      height={540}
+      // className="block md:hidden"
+      alt="Screenshot of the dashboard project showing mobile version"
+    />
+  </p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,13 @@
+import Image from "next/image";
 export default function Page() {
-  return <p>Invoices Page</p>;
+  return <p>
+    Invoices Page
+    <Image
+      src="/images/mela.png"
+      width={201 }
+      height={251}
+      // className="block md:hidden"
+      alt="Screenshot of the dashboard project showing mobile version"
+    />
+  </p>;
 }
